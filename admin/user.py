cf2946749c4aafd57a1540c3c62334e3b7aece8f
9394113857 <81347796+9394113857@users.noreply.github.com>
2021-07-06 16:12:51 +0530

    User Blueprint with logging executed

--- admin/user.py
+++ admin/user.py
@@ -1,28 +1,27 @@
 import re
 import socket
-# import logging
+import logging
 import MySQLdb
 from faker import Faker
 from flask import Blueprint, request, jsonify, Flask
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash
 
+# Create and configure logger:-
+logging.basicConfig(filename="F:\Restful-API's\RestAPI\log files\Signup.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='a')
 
-# # Create and configure logger:-
-# logging.basicConfig(filename="F:\Restful-API's\RestAPI\log files\Signup.log",
-#                     format='%(asctime)s %(message)s',
-#                     filemode='a')
-#
-# # Creating an object:-
-# logger = logging.getLogger()
-#
-# # Setting the threshold of logger to DEBUG:-
-# logger.setLevel(logging.DEBUG)
-#
-# # Test messages:-
-# logger.info('-----------------------------')
-# logger.info("User_Signup script started Now:-")
-# logger.info('-----------------------------')
+# Creating an object:-
+logger = logging.getLogger()
+
+# Setting the threshold of logger to DEBUG:-
+logger.setLevel(logging.DEBUG)
+
+# Test messages:-
+logger.info('-----------------------------')
+logger.info("User_Signup script started Now:-")
+logger.info('-----------------------------')
 
 
 # blueprint setup
@@ -30,153 +29,6 @@ user = Blueprint("user", __name__)
 
 app = Flask(__name__)
 mysql = MySQL(app)
-
-
-@user.route("/home")
-@user.route("/hom")
-def home():
-    return jsonify('Home page from home of /admin/home url of user blueprint instance !!!')
-
-
-@user.route("/test")
-def test():
-    return "<h1>Test</h1>"
-
-
-@user.route("/mysql")
-def mysql():
-    # Open databasee connection
-    mysql = MySQLdb.connect("localhost", "root", "", "clinicalfirst")
-
-    # prepare a cursor object using cursor() method
-    cursor = mysql.cursor()
-
-    # execute SQL query using execute() method.
-    cursor.execute("SELECT VERSION()")
-
-    # Fetch a single row using fetchone() method.
-    data = cursor.fetchone()
-
-    # print()
-    # print("Database version : %s " % data)
-
-    return jsonify('Database version : %s ' % data)
-
-    # disconnect from server
-    db.close()
-
-
-@user.route("/databases")
-def databases():
-    # Open databasee connection:-
-    mysql = MySQLdb.connect("localhost", "root", "", "clinicalfirst")
-    # mysql = MySQLdb.connections.cursors()
-
-    # prepare a cursor object using cursor() method:-
-    cursor = mysql.cursor()
-
-    # execute SQL query using execute() method:-
-    cursor.execute("SHOW DATABASES")
-
-    # Response:-
-    for x in cursor:
-        return jsonify(x[0])
-
-
-@user.route("/select")
-def select_method():
-    # Open databasee connection:-
-    mysql = MySQLdb.connect("localhost", "root", "", "clinicalfirst")
-    # mysql = MySQLdb.connections.cursors()
-
-    # prepare a cursor object using cursor() method:-
-    cursor = mysql.cursor()
-
-    # execute SQL query using execute() method:-
-    cursor.execute("SELECT * FROM user_signup")
-
-    myresult = cursor.fetchone()
-
-    return jsonify(myresult)
-
-
-@user.route("/insert")
-def insert_method():
-    # Open databasee connection:-
-    mysql = MySQLdb.connect("localhost", "root", "", "clinicalfirst")
-    # mysql = MySQLdb.connections.cursors()
-
-    # prepare a cursor object using cursor() method:-
-    cursor = mysql.cursor()
-
-    # execute SQL query using execute() method:-
-    cursor.execute("SELECT * FROM user_signup")
-
-    sql = "INSERT INTO user_signup (USER_ID, USER_NAME) VALUES (%s, %s)"
-    val = [
-        ('001', 'Raghu 2'),
-        ('002', 'Raghu 3')
-    ]
-
-    cursor.executemany(sql, val)
-
-    mysql.commit()
-
-    # print(cursor.rowcount, "record was inserted.")
-
-    # json response:-
-    return jsonify(cursor.rowcount, "record was inserted.")
-
-
-@user.route("/id")
-def id():
-    # Open databasee connection:-
-    mysql = MySQLdb.connect("localhost", "root", "", "clinicalfirst")
-    # mysql = MySQLdb.connections.cursors()
-
-    # prepare a cursor object using cursor() method:-
-    cursor = mysql.cursor()
-
-    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-    val = ("Michelle", "Blue Village")
-    cursor.execute(sql, val)
-
-    mysql.commit()
-
-    # print("1 record inserted, ID:", cursor.lastrowid)
-
-    return jsonify("1 record inserted, ID:", cursor.lastrowid)
-
-# @user.route("/insert", methods=['POST'])
-# def insert():
-#     # Open databasee connection:-
-#     mysql = MySQLdb.connect("localhost", "root", "", "clinicalfirst")
-#     # mysql = MySQLdb.connections.cursors()
-#
-#     userid = request.json
-#     user_id = userid['userid']
-#
-#     username = request.json
-#     user_name = username['username']
-#
-#     # prepare a cursor object using cursor() method:-
-#     cursor = mysql.cursor()
-#
-#     # execute SQL query using execute() method:-
-#     cursor.execute("INSERT INTO user_signup (USER_ID, USER_NAME) VALUES (%s, %s)", (user_id, user_name))
-#     mysql.commit()
-#
-#     return jsonify('Successfully Inserted'), 200
-#
-# """
-# Post man:-
-# POST:-
-# http://127.0.0.1:5000/db/insert
-# {
-#     "userid": "Us100",
-#     "username": "raghunadh"
-# }
-# """
 
 # User_Signup:-
 # create in postman by using jsonify:-
@@ -200,6 +52,7 @@ def register():
         account = cursor.fetchone()
         if account:
             msg = 'Account already exists !'
+            logger.info("User already exists: %s", username)
 
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', mail_id):
             msg = 'Invalid email address !'
@@ -246,6 +99,7 @@ def register():
             print('----------------------------------')
             print("Current Inserted ID is: " + str(current_user_id))
             print('----------------------------------')
+            logger.info("Successfully Registred with Id: %s", current_user_id)
             return "successfully inserted", 200
         return msg
     return "invalid parameters"
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 from admin.user import user
-# from databasee.database import db
-# from user_module.user import user
 
 app = Flask(__name__)
 
@@ -15,10 +13,6 @@ app.config['SECRET_KEY'] = 'secret_key'
 mysql = MySQL(app)
 
 app.register_blueprint(user, url_prefix="/user")
-# app.register_blueprint(db, url_prefix="/db")
-# app.register_blueprint(user, url_prefix="/user")
-
-
 
 # default root:-
 @app.route("/")
